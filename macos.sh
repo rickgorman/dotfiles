f2c0c9c9fc2ca2ec33c7180e99059a429de7aeec
Rick Gorman <rickgorman@users.noreply.github.com>
2023-03-28 09:25:57 -0700

    improve save dialog

--- macos.sh
+++ macos.sh
@@ -30,6 +30,12 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # Finder: show path bar
 defaults write com.apple.finder ShowPathbar -bool true
 
+# Finder: Expand save dialog by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
