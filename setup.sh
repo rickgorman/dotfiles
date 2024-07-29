1819c498d6c3b67cbb8f401331b6795ad6d5c3f7
Rick Gorman <rickgorman@users.noreply.github.com>
2024-07-28 22:11:48 -1000

    update VSCode files and symlink creation script

--- setup.sh
+++ setup.sh
@@ -28,6 +28,13 @@ ln -s ~/dotfiles-local/pryrc ~/.pryrc
 ln -s ~/dotfiles-local/rspec ~/.rspec
 ln -s ~/dotfiles-local/config/karabiner ~/.config/karabiner
 
+# VSCode things
+mv ~/Library/Application\ Support/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json.bak
+ln -s ~/dotfiles-local/code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json.bak
+ln -s ~/dotfiles-local/code/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+
 # this will rebuild symlinks; use from cli when needed
 rcup
 
