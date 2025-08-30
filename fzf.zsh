b1e6754e81c4a3260175fe39a85212224f43aea9
Rick Gorman <rickgorman@users.noreply.github.com>
2025-08-29 21:12:34 -0700

    simplify fzf.zsh

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+if [[ ! "$PATH" == */Users/me/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/Users/me/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
